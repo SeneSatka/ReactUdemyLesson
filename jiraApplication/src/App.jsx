0f26eef6bb850954e11ff535bf1ed96643af2f58
senesatka <senesatka@gmail.com>
2023-08-14 20:14:43 +0300

    jiraApplication useEffect ile db ye bağlandı  ve veri kaybı engellendi

--- jiraApplication/src/App.jsx
+++ jiraApplication/src/App.jsx
@@ -1,28 +1,40 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import "./App.css";
 import TaskCreate from "./Components/taskCreate";
 import TaskList from "./Components/TaskList";
+import axios from "axios";
 
 function App() {
   const [tasks, setTasks] = useState([]);
-  const createTask = (title, taskDesc) => {
-    const createdTasks = [
-      ...tasks,
-      {
-        id: Math.round(Math.random() * Date.now()),
-        title,
-        taskDesc,
-      },
-    ];
+
+  const createTask = async (title, taskDesc) => {
+    const res = await axios.post("http://localhost:3000/tasks", {
+      title,
+      taskDesc,
+    });
+    const createdTasks = [...tasks, res.data];
     setTasks(createdTasks);
   };
-  const deleteTaskById = (id) => {
+
+  const fetchTasks = async () => {
+    const res = await axios.get("http://localhost:3000/tasks");
+    setTasks(res.data);
+  };
+  useEffect(() => {
+    fetchTasks();
+  }, []);
+  const deleteTaskById = async (id) => {
+    await axios.delete(`http://localhost:3000/tasks/${id}`);
     const afterDeleteTask = tasks.filter((task) => {
       return task.id !== id;
     });
     setTasks(afterDeleteTask);
   };
-  const editTaskById = (id, updatedTitle, updatedDesc) => {
+  const editTaskById = async (id, updatedTitle, updatedDesc) => {
+    await axios.put(`http://localhost:3000/tasks/${id}`, {
+      title: updatedTitle,
+      taskDesc: updatedDesc,
+    });
     const updatedTasks = tasks.map((task) => {
       if (task.id == id)
         return { id, title: updatedTitle, taskDesc: updatedDesc };
