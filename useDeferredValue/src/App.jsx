16c2bd776b578c20bf536ff87138fac076902d4a
senesatka <senesatka@gmail.com>
2023-08-16 14:26:29 +0300

    useDeferredValue kullanımı

---
+++ useDeferredValue/src/App.jsx
@@ -0,0 +1,18 @@
+import { useState } from "react";
+import "./App.css";
+import Characters from "./Characters";
+
+function App() {
+  const [input, setInput] = useState("");
+  const handleChange = (e) => {
+    setInput(e.target.value);
+  };
+  return (
+    <>
+      <input type="text" value={input} onChange={handleChange} />
+      <Characters input={input} />
+    </>
+  );
+}
+
+export default App;
