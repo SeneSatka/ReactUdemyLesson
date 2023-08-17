70f25d07dffd69235a55a8ccaafdf703c5c11b3c
senesatka <senesatka@gmail.com>
2023-08-17 13:46:48 +0300

    53. Ders Link

--- Router/src/App.jsx
+++ Router/src/App.jsx
@@ -2,9 +2,11 @@ import "./App.css";
 import { Routes, Route } from "react-router-dom";
 import Home from "./Components/Home";
 import About from "./Components/About";
+import Navbar from "./Components/Navbar";
 function App() {
   return (
     <div className="App">
+      <Navbar />
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/about" element={<About />} />
