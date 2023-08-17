a2811d834b942a76131a724245087c7e31bf4528
senesatka <senesatka@gmail.com>
2023-08-17 14:31:15 +0300

    59.Ders useSearchParams

--- Router/src/Components/Members.jsx
+++ Router/src/Components/Members.jsx
@@ -1,7 +1,19 @@
 import React from "react";
+import { useSearchParams } from "react-router-dom";
 
 function Members() {
-  return <div>Members</div>;
+  const [searchParams, setSearchParams] = useSearchParams();
+  const isActive = searchParams.get("filter") === "active";
+  return (
+    <>
+      <div>Members</div>
+      <button onClick={() => setSearchParams({ filter: "active" })}>
+        Aktif üyeler
+      </button>
+      <button onClick={() => setSearchParams()}>Tüm üyeler</button>
+      {isActive ? <h2>Aktif Üyeler</h2> : <h2>Tüm üyeler</h2>}
+    </>
+  );
 }
 
 export default Members;
